b75f94bd8c8eb6144181a7d19d52779896d839dd
bertli <lmtmeet2007@163.com>
2018-04-21 16:40:23 +0800

    add date util

---
+++ NetCommon.Console/Common/DateUtil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NetCommon.Console.Common
+{
+    public class DateUtil
+    {
+        public static int GetWorkDays(DateTime startDate, DateTime endDate)
+        {
+            TimeSpan ts = endDate.Subtract(startDate);
+            int countday = ts.Days;//获取两个日期间的总天数  
+            int weekday = 0;//工作日  
+            //循环用来扣除总天数中的双休日  
+            for (int i = 0; i < countday + 1; i++)
+            {
+                DateTime tempdt = startDate.Date.AddDays(i);
+                if (tempdt.DayOfWeek != DayOfWeek.Saturday && tempdt.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    weekday++;
+                }
+            }
+            return weekday;
+        }
+    }
+}
